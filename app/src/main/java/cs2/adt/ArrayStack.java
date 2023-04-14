c552183fea2464ea2fa63444678e90035539ad5b
Matt Hibbs <mhibbs@trinity.edu>
2023-04-14 12:06:10 -0500

    Finished ArrayStack, started StackTester

--- app/src/main/java/cs2/adt/ArrayStack.java
+++ app/src/main/java/cs2/adt/ArrayStack.java
@@ -10,8 +10,23 @@ public class ArrayStack<T> implements Stack<T> {
   }
 
 
-  public void push(T item);
-  public T pop();
-  public T peek();
-  public boolean isEmpty();  
+  public void push(T item) {
+    if(len == arr.length) {
+      T[] tmp = (T[]) new Object[len*2];
+      for(int i=0; i<len; i++) {
+        tmp[i] = arr[i];
+      }
+      arr = tmp;
+    }
+    arr[len] = item;
+    len++;
+  }
+  public T pop() {
+    len--;
+    return arr[len];
+  }
+  public T peek() {
+    return arr[len-1];
+  }
+  public boolean isEmpty() { return len == 0; }  
 }
